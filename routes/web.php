787bcef0466390c177358bf66680b918064b38ff
tamilvanan <skjsinfo@gmail.com>
2026-09-21 18:48:03 +0530

    gateway method

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\View\Components\PaymentMethod;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/payment-method', [PaymentMethod::class, 'index'])->name('payment-method');
+Route::get('/login', function () {
+    return view('components.login-page');
+})->name('login');
