68c4d43578e2d011dcc5e160442624005ba6ea15
tamilvanan <skjsinfo@gmail.com>
2026-09-25 19:36:41 +0530

    scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Payment;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('payments:mark-expired')
+    ->everyMinute()
+    ->withoutOverlapping();
